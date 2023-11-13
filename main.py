cfa7af0250d2356d60e61ef18b077de89caba1ae
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-11-13 18:02:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Dont stop, keep practising..'
+    return 'Dont stop, keep practising../n you see has you keep practising you making mistakes but you are learning from it'
