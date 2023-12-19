a03461768a23cec8a39c5d1e9f36c473f74f0fc6
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-19 10:31:51 +0100

    vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,8 +1,15 @@
-// create vpc
-
+//create a VPC 
 resource "google_compute_network" "network" {
-name          = "network-connect"
-ip_cidr_range = "192.168.1.0/24
-network       = google_compute_network.network.self_link
-region        = var.region
+  name                        = "net-connect"
+  auto_create_subnetworks     = "false"
+}
+
+// create subnet
+
+resource "google_compute_subnetwork" "my_subnet"{
+name           = "subnet-connect"
+ip_cidr_range  = "192.168.1.0/24"
+  network      = google_compute_network.network.self_link
+  region       = var.region
+
 }
