6468e4e7d153567ce7c6a2137b4a22279dc9ae15
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-19 10:25:41 +0100

    vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,6 +3,6 @@
 resource "google_compute_network" "network" {
 name          = "network-connect"
 ip_cidr_range = "192.168.1.0/24
-
-
+network       = google_compute_network.network.self_link
+region        = var.region
 }
